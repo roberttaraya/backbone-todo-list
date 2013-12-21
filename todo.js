d379e3a8d5dafe7351951164f26f846de19aa461
Robert Taraya <robert.taraya@gmail.com>
2013-12-21 01:02:28 -0600

    create todo view

--- todo.js
+++ todo.js
@@ -9,3 +9,37 @@ var myTodo = new Todo({
   title: 'Check attributes property of the logged models in the console.'
 })
 
+var TodoView = Backbone.View.extend({
+
+  tagName:  'li',
+
+  todoTpl: _.template( $('#item-template').html() ),
+
+  events: {
+    'dblclick label': 'edit',
+    'keypress .edit': 'updateOnEnter',
+    'blur .edit':   'close'
+  },
+
+  initialize: function() {
+    this.$el = $('#todo');
+  },
+
+  render: function() {
+    this.$el.html( this.todoTpl( this.model.toJSON() ) );
+
+    this.input = this.$('.edit');
+    return this;
+  },
+
+  edit: function() {
+  },
+
+  close: function() {
+  },
+
+  updateOnEnter: function( e ) {
+  }
+});
+
+var todoView = new TodoView({model: myTodo});
